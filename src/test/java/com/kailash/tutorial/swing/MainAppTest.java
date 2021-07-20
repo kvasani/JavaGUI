42893b493eefa46bf8f55a3298dcba0d864f95ca
Kailash Vasani <kvasani@gmail.com>
2021-07-19 23:02:28 -0400

    added test packages

---
+++ src/test/java/com/kailash/tutorial/swing/MainAppTest.java
@@ -0,0 +1,24 @@
+package com.kailash.tutorial.swing;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainAppTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+        String[] args = new String[] {};
+        com.kailash.tutorial.swing.MainApp.main(args);
+    }
+}
